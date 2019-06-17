40fc55585dfd23999ba37d66f0cd2159abd870c3
maxmcd <max.t.mcdonnell@gmail.com>
2019-06-17 11:59:42 -0400

    Add embly docs and rustlib 0.0.2

--- embly/src/lib.rs
+++ embly/src/lib.rs
@@ -1,4 +1,13 @@
-//! embly is
+//! Embly is a lightweight application runtime. It runs small isolated programs. Let's call
+//! these programs "sparks". Sparks can do a handful of things:
+//!
+//! - Receive bytes
+//! - Send bytes
+//! - Spawn a new spark
+//!
+//! This library is used to access embly functionality from within a program
+//! it is intended to only be built with `wasm32-wasi` but compilation should
+//! work with other targets
 
 #![deny(
     missing_docs,
@@ -29,7 +38,59 @@
 use std::io;
 use std::io::Result;
 
-/// Comm is something without documentation yet
+/// Comm's handle communication between functions or to the function caller
+///
+/// ## Receive Bytes
+///
+/// When a spark begins execution it can optionally read in any bytes that it might have
+/// been sent. Maybe there are bytes ready on startup, maybe it'll receive them later.
+///
+/// Over time, a spark can receive multiple messages. Maybe parts of a request body or
+/// various incremental updates. Each separate message will be separated by an `io::EOF`
+/// error.
+///
+/// ```rust
+/// use embly::Comm;
+/// use std::io;
+/// use std::io::Read;
+///
+/// fn entrypoint(mut comm: Comm) -> io::Result<()> {
+///     let mut buffer = Vec::new();
+///     // Comm implements std::io::Read
+///     comm.read_to_end(&mut buffer)?;
+///
+///     // a little while later you might get another message
+///     comm.read_to_end(&mut buffer)?;
+///     return Ok(())
+/// }
+/// ```
+///
+/// ## Write Bytes
+///
+/// Writes can be written back. A spark is always executed by something. This could be a
+/// command line call, a load balancer or another spark. Writing to a comm will send
+/// those bytes back to the spark runner.
+///
+/// ```rust
+/// use embly::Comm;
+/// use std::io;
+/// use std::io::Write;
+///
+/// fn entrypoint(mut comm: Comm) -> io::Result<()> {
+///     // you can call write_all to send one message
+///     comm.write_all("Hello World".as_bytes())?;
+///
+///
+///     // Or you can make multiple calls with write if you want to construct a
+///     // message and then flush the response
+///     comm.write(b"Hello")?;
+///     comm.write(b"World")?;
+///     comm.flush()?;
+///     return Ok(())
+/// }
+/// ```
+///
+///
 pub struct Comm {
     id: i32,
 }
@@ -48,54 +109,66 @@ impl io::Write for Comm {
     }
 }
 
-/// spawn a function!
-pub struct Function {}
-
-impl Function {}
-
 #[cfg(all(target_arch = "wasm32"))]
 extern "C" {
-    fn _read(id: i32, payload: *const u8, payload_len: u32, ln: *const i32) -> u32;
-    fn _write(id: i32, payload: *const u8, payload_len: u32, ln: *const i32) -> u32;
+    fn _read(id: i32, payload: *const u8, payload_len: u32, ln: *mut i32) -> u32;
+    fn _write(id: i32, payload: *const u8, payload_len: u32, ln: *mut i32) -> u32;
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-unsafe fn _read(_id: i32, _payload: *const u8, _payload_len: u32, _ln: *const i32) -> u32 {
+unsafe fn _read(_id: i32, _payload: *const u8, payload_len: u32, ln: *mut i32) -> u32 {
+    // lie and say we read things
+    *ln = payload_len as i32;
     return 0;
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-unsafe fn _write(_id: i32, _payload: *const u8, _payload_len: u32, _ln: *const i32) -> u32 {
+unsafe fn _write(_id: i32, _payload: *const u8, payload_len: u32, ln: *mut i32) -> u32 {
+    // lie and say we write things
+    *ln = payload_len as i32;
     return 0;
 }
 
-/// read something
-pub fn read(id: i32, payload: &mut [u8]) -> Result<usize> {
-    let ln: i32 = 0;
-    let ln_ptr: *const i32 = &ln;
+fn read(id: i32, payload: &mut [u8]) -> Result<usize> {
+    let mut ln: i32 = 0;
+    let ln_ptr: *mut i32 = &mut ln;
     let err = unsafe { _read(id, payload.as_ptr(), payload.len() as u32, ln_ptr) };
     println!("read err {:?}", err);
     Ok(ln as usize)
 }
 
-/// write something
-pub fn write(id: i32, payload: &[u8]) -> Result<usize> {
-    let ln: i32 = 0;
-    let ln_ptr: *const i32 = &ln;
+fn write(id: i32, payload: &[u8]) -> Result<usize> {
+    let mut ln: i32 = 0;
+    let ln_ptr: *mut i32 = &mut ln;
     let err = unsafe { _write(id, payload.as_ptr(), payload.len() as u32, ln_ptr) };
     println!("read err {:?}", err);
     Ok(ln as usize)
 }
-/// sfasDFaf
+
+/// Run a function
+///
+/// ```
+///
+/// use std::io;
+/// use std::io::Read;
+/// use std::io::Write;
+///
+/// fn execute(mut comm: embly::Comm) -> io::Result<()> {
+///     comm.write_all(b"Hello\n")?;
+///     let mut out = Vec::new();
+///     comm.read_to_end(&mut out)?;
+///     println!("{:?}", out);
+///     Ok(())
+/// }
+/// fn main() {
+///     embly::run(execute);
+/// }
+/// ```
+///
 pub fn run(to_run: fn(Comm) -> io::Result<()>) {
     let c = Comm { id: 1 };
     to_run(c).unwrap();
-    println!("i'm running are you")
 }
-
-#[no_mangle]
-/// start
-pub extern "C" fn start(_len: i32) {}
 
 #[cfg(test)]
 mod tests {
