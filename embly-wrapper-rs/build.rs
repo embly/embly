d16ec0c7b69ac8c00474d6d710b2978328a5f934
maxmcd <max.t.mcdonnell@gmail.com>
2020-01-11 17:14:41 -0500

    wrapper 0.0.2

--- embly-wrapper-rs/build.rs
+++ embly-wrapper-rs/build.rs
@@ -1,15 +1,15 @@
-extern crate protoc_rust;
+// extern crate protoc_rust;
 
-use protoc_rust::Customize;
+// use protoc_rust::Customize;
 
 fn main() {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/protos",
-        input: &["../pkg/core/proto/comms.proto"],
-        includes: &["../pkg/core/proto/"],
-        customize: Customize {
-            ..Default::default()
-        },
-    })
-    .expect("protoc");
+    // protoc_rust::run(protoc_rust::Args {
+    //     out_dir: "src/protos",
+    //     input: &["../pkg/core/proto/comms.proto"],
+    //     includes: &["../pkg/core/proto/"],
+    //     customize: Customize {
+    //         ..Default::default()
+    //     },
+    // })
+    // .expect("protoc");
 }
